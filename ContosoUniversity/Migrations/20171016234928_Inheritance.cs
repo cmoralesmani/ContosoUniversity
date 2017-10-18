9e7430180ed228a9679c542ad06b8284386c88c2
Cesar Augusto Morales <cmorales@thedataage.com>
2017-10-17 18:00:39 -0600

    Add migration

--- ContosoUniversity/Migrations/20171016234928_Inheritance.cs
+++ ContosoUniversity/Migrations/20171016234928_Inheritance.cs
@@ -10,95 +10,37 @@ namespace ContosoUniversity.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
-                name: "FK_CourseAssignment_Instructor_InstructorID",
-                table: "CourseAssignment");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_Department_Instructor_InstructorID",
-                table: "Department");
-
-            migrationBuilder.DropForeignKey(
                 name: "FK_Enrollment_Student_StudentID",
                 table: "Enrollment");
 
-            migrationBuilder.DropForeignKey(
-                name: "FK_OfficeAssignment_Instructor_InstructorID",
-                table: "OfficeAssignment");
+            migrationBuilder.DropIndex(name: "IX_Enrollment_StudentID", table: "Enrollment");
+
+            migrationBuilder.RenameTable(name: "Instructor", newName: "Person");
+            migrationBuilder.AddColumn<DateTime>(name: "EnrollmentDate", table: "Person", nullable: true);
+            migrationBuilder.AddColumn<string>(name: "Discriminator", table: "Person", nullable: false, maxLength: 128, defaultValue: "Instructor");
+            migrationBuilder.AlterColumn<DateTime>(name: "HireDate", table: "Person", nullable: true);
+            migrationBuilder.AddColumn<int>(name: "OldId", table: "Person", nullable: true);
+
+            // Copy existing Student data into new Person table.
+            migrationBuilder.Sql("INSERT INTO \"Person\" (\"LastName\", \"FirstName\", \"HireDate\", \"EnrollmentDate\", \"Discriminator\", \"OldId\") SELECT \"LastName\", \"FirstName\", null AS HireDate, \"EnrollmentDate\", 'Student' AS Discriminator, \"ID\" AS OldId FROM \"Student\"");
+            // Fix up existing relationships to match new PK's.
+            migrationBuilder.Sql("UPDATE \"Enrollment\" SET \"StudentID\" = (SELECT \"ID\" FROM \"Person\" WHERE \"OldId\" = \"StudentID\" AND \"Discriminator\" = 'Student')");
+
+            // Remove temporary key
+            migrationBuilder.DropColumn(name: "OldID", table: "Person");
 
             migrationBuilder.DropTable(
-                name: "Instructor");
+                name: "Student");
 
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_Student",
-                table: "Student");
-
-            migrationBuilder.RenameTable(
-                name: "Student",
-                newName: "Person");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "LastName",
-                table: "Person",
-                type: "varchar(50)",
-                maxLength: 50,
-                nullable: false,
-                oldClrType: typeof(string),
-                oldMaxLength: 50,
-                oldNullable: true);
-
-            migrationBuilder.AlterColumn<DateTime>(
-                name: "EnrollmentDate",
-                table: "Person",
-                type: "timestamp",
-                nullable: true,
-                oldClrType: typeof(DateTime));
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "HireDate",
-                table: "Person",
-                type: "timestamp",
-                nullable: true);
-
-            migrationBuilder.AddColumn<string>(
-                name: "Discriminator",
-                table: "Person",
-                type: "text",
-                nullable: false,
-                defaultValue: "");
-
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_Person",
-                table: "Person",
-                column: "ID");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_CourseAssignment_Person_InstructorID",
-                table: "CourseAssignment",
-                column: "InstructorID",
-                principalTable: "Person",
-                principalColumn: "ID",
-                onDelete: ReferentialAction.Cascade);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Department_Person_InstructorID",
-                table: "Department",
-                column: "InstructorID",
-                principalTable: "Person",
-                principalColumn: "ID",
-                onDelete: ReferentialAction.Restrict);
+            migrationBuilder.CreateIndex(
+                 name: "IX_Enrollment_StudentID",
+                 table: "Enrollment",
+                 column: "StudentID");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_Enrollment_Person_StudentID",
                 table: "Enrollment",
                 column: "StudentID",
-                principalTable: "Person",
-                principalColumn: "ID",
-                onDelete: ReferentialAction.Cascade);
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_OfficeAssignment_Person_InstructorID",
-                table: "OfficeAssignment",
-                column: "InstructorID",
                 principalTable: "Person",
                 principalColumn: "ID",
                 onDelete: ReferentialAction.Cascade);
--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using ContosoUniversity.Data;
+using Microsoft.Extensions.Configuration.CommandLine;
 
 namespace ContosoUniversity
 {
@@ -17,8 +18,14 @@ namespace ContosoUniversity
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
+            Seeds(host);
 
-            using(var scope = host.Services.CreateScope())
+            host.Run();
+        }
+
+        public static void Seeds(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 try
@@ -26,19 +33,26 @@ namespace ContosoUniversity
                     var context = services.GetRequiredService<SchoolContext>();
                     DbInitializer.Initialize(context);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occurred while seeding the database.");
                 }
             }
-
-            host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var configurations = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            return WebHost.CreateDefaultBuilder()
+                .UseConfiguration(configurations)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
+        }
     }
 }
